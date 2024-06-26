fd1478f9b6bdd0529f3dd5a4c8d1e995589eae7b
diogo <diogovanzosabec@gmail.com>
2024-06-26 13:22:27 -0300

    chore: Update layout and responsiveness of home page

--- templates/static/home/js/headerManager.js
+++ templates/static/home/js/headerManager.js
@@ -73,12 +73,12 @@ class MainFooter extends HTMLElement {
         <!--Grid row-->
         <div class="row p-1 align-items-center justify-content-center">
             <!--Grid column for Logo-->
-            <div class="col-6 col-lg-2 text-md-left mb-3 mb-md-0">
-                <img src="${logoPar}" class="img-fluid">
+            <div class="col-lg-2 text-md-left mb-3 mb-md-0">
+                <img src="${logoPar}" height="100px" class="img-fluid d-none d-md-block">
             </div>
 
             <!--Grid column for Address-->
-            <div class="col-12 col-md-4 col-lg-2 mb-3 mb-md-0">
+            <div class="col-6 col-md-4 col-lg-2 mb-3 mb-md-0">
                 <h5 class="text-uppercase">Endereço</h5>
                 <ul class="list-unstyled">
                     <li>Paróquia Coração de Maria</li>
@@ -89,7 +89,7 @@ class MainFooter extends HTMLElement {
             </div>
 
             <!--Grid column for Useful Links-->
-            <div class="col-12 col-md-4 col-lg-2 mb-3 mb-md-0">
+            <div class="col col-md-auto col-lg-2 mb-3 mb-md-0">
                 <h5 class="text-uppercase">Links Úteis</h5>
                 <ul class="list-unstyled">
                     <li><a href="https://www.instagram.com/rcc.londrina/" class="navbarlink text-white text-decoration-none">
@@ -103,7 +103,7 @@ class MainFooter extends HTMLElement {
             </div>
 
             <!--Grid column for More About Us-->
-            <div class="col-12 col-md-4 col-lg-2 mb-3 mb-md-0">
+            <div class="col-6 col-md-4 col-lg-2 mb-3 mb-md-0 justify-col-content-center">
                 <h5 class="text-uppercase">Veja mais sobre nós:</h5>
                 <ul class="list-unstyled">
                     <li><a href="https://www.instagram.com/maranathaoficial/" class="text-white text-decoration-none"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16"><path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/></svg>
@@ -115,7 +115,7 @@ class MainFooter extends HTMLElement {
 
             <!--Grid column for RCC Logo-->
             <div class="col-6 col-md-3 col-lg-2">
-                <img src="${logoRcc}" height="80px" class="img-fluid">
+                <img src="${logoRcc}" height="80px" class="img-fluid d-none d-md-block">
             </div>
         </div>
         <!--Grid row-->
