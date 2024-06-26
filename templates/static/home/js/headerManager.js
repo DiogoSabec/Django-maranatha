ea31998e5202866bdfe6818bddcc8435dd40cdcf
diogo <diogovanzosabec@gmail.com>
2024-06-26 16:12:58 -0300

    chore: Update home page layout and styles

--- templates/static/home/js/headerManager.js
+++ templates/static/home/js/headerManager.js
@@ -12,13 +12,18 @@ class MainHeader extends HTMLElement {
         <div class="container-fluid fill bg-secondary" style="height: 100px; display: flex; align-items: center;" id="topo">
             <div class="container">
                 <nav class="navbar navbar-expand-md" style="width: 100%; display: flex; align-items: center;">
+                    
                     <a href="${homeUrl}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                        <img src="${logoUrl}" width="50" height="40"> <!-- Use logo URL here -->
-                        <span class="fs-2 text-white">aranatha</span>
+                        <div class="container justify-content-end">    
+                            <img src="${logoUrl}" width="50" height="40">
+                            <span class="fs-2 text-white">aranatha</span>
+                        </div>    
                     </a>
+
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
+
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item"><a href="${homeUrl}" class="${this.isActive(homeUrl, currentPath)} fs-5 me-1 btn btn-success nav-link" aria-current="page">Home</a></li>
@@ -28,9 +33,11 @@ class MainHeader extends HTMLElement {
                             <li class="nav-item"><a href="${contactsUrl}" class="${this.isActive(contactsUrl, currentPath)} fs-5 me-1 btn btn-success nav-link">Contatos</a></li>
                         </ul>
                     </div>
+
                 </nav>
+                
             </div>
-        </div>
+        </div>   
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 50" preserveAspectRatio="none" style="display: block;">
     <path fill="#71bfff" opacity="0.33" d="M473,33.65 c-203.9,44.15-263.1-17-320.3,0C66,59.55,0,29.85,0,29.85V0h1000v29.85 c0,0-62.1,13.05-94.9,14.65c-32.8,1.65-62.8-6.15-75.8-11.05C806,24.8,745.3,4.35,694.9,2.35S492.4,29.5,473,33.65z"></path>
     <path fill="#71bfff" opacity="0.66" d="M734,33.65 c-45.5,0-77.2-11.6-129.1-19.55c-28.6-4.35-150.3-5.05-254,19.55 s-91.7-17.2-149.2,0C115.7,59.15,0,19.9,0,19.9V0h1000v18.25c0,0-28.2-9.25-92.1-9.25C810.2,9.05,775.7,33.65,734,33.65z"></path>
@@ -66,7 +73,7 @@ class MainFooter extends HTMLElement {
         const logoRcc = this.getAttribute('rcc-logo-url');
 
         this.innerHTML = `
-<footer class="bg-primary text-white text-center text-lg-start">
+<footer class="bg-primary text-white text-center text-lg-start"id="footer">
     <!-- Grid container -->
     <div class="container-fluid p-2">
 
@@ -128,7 +135,6 @@ class MainFooter extends HTMLElement {
     </div>
     <!-- Copyright -->
 </footer>
-
         `;
     }
 }
